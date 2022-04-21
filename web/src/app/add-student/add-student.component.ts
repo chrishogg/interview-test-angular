9d102ed22f94bb76c597c372ffc6bf4fb358d19f
Chris Hogg <chogg@waikato.ac.nz>
2022-04-22 10:56:10 +1200

    Added stub files for add student page.

---
+++ web/src/app/add-student/add-student.component.ts
@@ -0,0 +1,10 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-add-student',
+  templateUrl: './add-student.component.html',
+})
+export class AddStudentComponent {
+  
+}
