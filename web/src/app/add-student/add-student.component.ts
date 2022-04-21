8f11ed55d4d098743b01f1b9d1a3675a074446dd
Chris Hogg <chogg@waikato.ac.nz>
2022-04-22 11:56:54 +1200

    Added html template for the new student form and onSubmitTemplateBased method of the AddStudentComponent class. Also added functionality to make a http post back to the dotnet api.

--- web/src/app/add-student/add-student.component.ts
+++ web/src/app/add-student/add-student.component.ts
@@ -1,10 +1,27 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormControl } from '@angular/forms';
+//import the student interface from the module.
+import { Student } from '../app.module';
 
 @Component({
   selector: 'app-add-student',
   templateUrl: './add-student.component.html',
 })
 export class AddStudentComponent {
-  
+
+  //create a object to store our user form data
+  user: Object = {};
+  http: HttpClient;
+  baseUrl: string;
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.http = http;
+    this.baseUrl = baseUrl;
+  }
+
+  public onSubmitTemplateBased(user) {
+      console.log(user);
+      this.http.post<Student>(this.baseUrl + 'students/add', user).subscribe(result => console.error(result), error => console.error(error));
+  }
 }
